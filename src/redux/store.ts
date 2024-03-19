dfafa61601962621af2d97a99be86384925d79a0
apatil3142 <patilaniket2236@gmail.com>
2024-03-19 22:17:12 +0530

    Redux persist to memoise state on refresh

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import ToDoSlice from './ToDoSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
-const store = configureStore({
-  reducer: {
-    todo: ToDoSlice
-  }
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({ todo: ToDoSlice});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ReturnType<typeof store.dispatch>
-export default store;
+export const persistor = persistStore(store);
